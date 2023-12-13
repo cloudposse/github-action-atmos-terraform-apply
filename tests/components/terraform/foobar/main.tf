75e5571afbe59466b0a8c79ff4ba78adffb8ddf0
Igor Rodionov <goruha@gmail.com>
2023-12-13 18:49:40 +0300

    Move to GitOps Config File (#29)
    ## What
    
    * Use configuration file
    
    ## Why
    
    *  Make DRY workflows

--- tests/components/terraform/foobar/main.tf
+++ tests/components/terraform/foobar/main.tf
@@ -2,6 +2,7 @@ resource "random_id" "foo" {
   keepers = {
     # Generate a new id each time we switch to a new seed
     seed = "${module.this.id}-${var.example}"
+    timestamp = "${timestamp()}"
   }
   byte_length = 8
 }

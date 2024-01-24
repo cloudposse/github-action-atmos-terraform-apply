d333b062f3c81106f23f53e27af27df6829e81a7
Igor Rodionov <goruha@gmail.com>
2024-01-24 22:09:37 +0100

    Added test-workflows

--- tests/components/terraform/foobar/outputs.tf
+++ tests/components/terraform/foobar/outputs.tf
@@ -2,3 +2,9 @@ output "result" {
   description = "Test description output"
   value       = random_id.foo.id
 }
+
+output "sensitive_value" {
+  description = "Test sensitive description output"
+  value       = random_id.foo.id
+  sensitive   = true
+}
